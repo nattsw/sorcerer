9791d4a7aa105aa8ea0fe07833d5d38a7abf2771
Natalie Tay <natalie.tay@gmail.com>
2016-10-16 22:35:03 +0800

    Some exceptions

--- pull.rb
+++ pull.rb
@@ -13,10 +13,26 @@ links = session.find_all('.entry-content li a').map { |l| l[:href] }
 
 links.each do |link|
   session.visit link
-
-  script = session.find('script[type="application/ld+json"]', visible: false)
-  content = JSON.parse(script.text(:all))
-  video_link = content['@id']
-  YoutubeDL.download(video_link)
+  begin
+    script = session.find('script[type="application/ld+json"]', visible: false)
+    content = JSON.parse(script.text(:all))
+    video_link = content['@id']
+    YoutubeDL.download(video_link)
+  rescue
+    begin
+      script = session.first('p script', visible: false)
+      content = script['src']
+      video_link = content.sub('medias', 'iframe').sub('.jsonp', '').sub('com', 'net')
+      if video_link != 'https://fast.wistia.net/assets/external/E-v1.js'
+        YoutubeDL.download(video_link)
+      else
+        title = session.first('h1.entry-title').text.sub('Lesson ', '0').sub(':', ' -') + '.mp4'
+        video_link = session.first('source', visible: false)['src']
+        YoutubeDL.download(video_link, output: title)
+      end
+    rescue
+      puts "You gotta debug this #{link} yourself, sadly"
+    end
+  end
 end
 
